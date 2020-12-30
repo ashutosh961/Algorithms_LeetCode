aa29e44ce9af095a54c6ebee052d2dbd889f4a86
Ashutosh Upadhye <ashutosh.upadhye96@gmail.com>
2020-12-29 22:06:02 -0800

    0 ms

---
+++ solutions/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int profit1 = 0;
+        int profit2 = 0;
+        
+        for(int i=1; i<prices.length; i++)
+        {
+            int copy = profit1;
+            profit1 = Math.max(profit2, profit1 + prices[i] - prices[i-1]);
+            profit2 = Math.max(copy, profit2);
+        }
+    
+        return Math.max(profit1, profit2);
+    }
+    
+}
